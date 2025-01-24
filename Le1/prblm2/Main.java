3b30862d7d81b0f1356b4ac4d526446f8bb1f900
221B209 <221B209@juet.co.in>
2025-01-24 14:38:34 +0530

    completing Le1 excercise #4

---
+++ Le1/prblm2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Mother obj1 = new Mother();
+        obj1.show(); 
+
+        Child obj2 = new Child();
+        obj2.show(); 
+    }
+}
---
+++ Le1/prblm4/Two.java
@@ -0,0 +1,8 @@
+class Two extends One {
+    
+    // when this will be empty it will cause compliation error 
+    
+    Two(int x) {
+        super(x);
+    }
+}
