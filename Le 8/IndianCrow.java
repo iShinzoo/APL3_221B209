1b2beebd7a9a9baebf69fb9348b98ef6072c98f8
221B209 <221B209@juet.co.in>
2025-04-25 14:27:13 +0530

    feat: add Lab exercise 8

---
+++ Le 8/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void eat(){
+        System.out.println("pearl ");
+    }
+    public void cry(){
+        System.out.println("caow ");
+    }
+    public void fly(){
+        System.out.println("flying ");
+    }
+}
